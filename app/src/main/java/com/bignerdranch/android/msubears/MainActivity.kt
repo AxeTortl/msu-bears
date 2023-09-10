56749e6e9adc43b9d6d3282f25245a9b4611ef50
AxeTortl <91918940+AxeTortl@users.noreply.github.com>
2023-09-10 17:32:18 -0500

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/msubears/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.msubears
+// com.esslinger.msu.msubears
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
